f9ad539b51721cf31b0ccfc5da0cd21533b2a936
zhangzly <dear531@21cn.com>
2018-07-02 20:11:55 +0800

    datatype long

---
+++ dataTypeLong.java
@@ -0,0 +1,13 @@
+class dataTypeLong
+{
+	public static void main(String[] args)
+	{
+		/* 超过int型数值，加L或l */
+		long a = 100000000000000L;
+		System.out.println(a);
+		long b = 100;
+		System.out.println(b);
+		long timeMillis = System.currentTimeMillis();
+		System.out.println(timeMillis);
+	}
+}
