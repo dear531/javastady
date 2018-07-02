c2a4492b3871fea8fd7ad6066e6dd1ff6dbfd7c8
zhangzly <dear531@21cn.com>
2018-07-02 20:58:57 +0800

    data type double

---
+++ dataTypeDouble.java
@@ -0,0 +1,10 @@
+class dataTypeDouble
+{
+	public static void main(String[] args)
+	{
+		double a = 3.0;
+		float b = 2.9F;
+		System.out.println("a:"+a+",b:"+b);
+		System.out.println(3 - 2.9);
+	}
+}
