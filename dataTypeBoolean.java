87859b0a0c40a5fe7695110903e894ce08c7b873
zhangzly <dear531@21cn.com>
2018-07-02 21:17:01 +0800

    data type boolean

---
+++ dataTypeBoolean.java
@@ -0,0 +1,9 @@
+class dataTypeBoolean
+{
+	public static void main(String[] args)
+	{
+		boolean flag1 = true;
+		boolean flag2 = false;
+		System.out.println("flag1:"+flag1+",flag2:"+flag2);
+	}
+}
