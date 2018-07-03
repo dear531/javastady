2aff06dbb18f15a8a491005a2a0c4df4a96c4017
zhangzly <dear531@21cn.com>
2018-07-03 19:26:53 +0800

    operator

---
+++ operator.java
@@ -0,0 +1,11 @@
+class operator
+{
+	public static void main(String[] args)
+	{
+		/* 整数相除，结果只取整数部分 */
+		System.out.println(10/3);
+		/* 小数相除，结果取小数结果 */
+		System.out.println(10.2/3);
+		System.out.println(9.0/4);
+	}
+}
