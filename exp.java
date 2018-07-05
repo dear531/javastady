e9bcd553c0e9faa4a5c5dac460bc35c17b21f0e5
zhangzly <dear531@21cn.com>
2018-07-05 20:29:25 +0800

    link

--- exp.java
+++ exp.java
@@ -3,9 +3,14 @@ class exp
 	public static void main(String[] args)
 	{
 		/* 条件表达式的值的类型必须为boolean */
+		short a = 1;
 		if ((1 == 0) == false)
 		{
 			System.out.println(1);
 		}
+		a = 1;
+		System.out.println(a);
+		System.out.println("hello"+1+2+3);
+		System.out.println(1+2+3+"hello");
 	}
 }
