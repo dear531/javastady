dd6c759b0dd230cb72c6804f2da2f5d75bd60a90
zhangzly <dear531@21cn.com>
2018-08-31 21:34:46 +0800

    usage of final

---
+++ Final.java
@@ -0,0 +1,8 @@
+class Final {
+	public static void main(String[] args) {
+		final double pi = 3.14;
+		/* can't give value to it second, below it's wrong */
+		/* pi = 3.15 */
+		System.out.println(pi);
+	}
+}
