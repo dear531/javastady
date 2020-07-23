1a502c68daf4faca244953eccc507393fd2584eb
zhangzly <dear531@21cn.com>
2020-07-23 22:53:43 +0800

    add foreach

---
+++ For.java
@@ -0,0 +1,11 @@
+public class For {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		int array[] = {21, 23, 25};
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
+}
