eaf8397fe9d3ecee9862833946ca505dfebec7f2
zhangzly <dear531@21cn.com>
2020-07-28 22:05:56 +0800

    add string concatenate int

---
+++ Concat.java
@@ -0,0 +1,13 @@
+public class Concat {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s1 = "hello";
+		String s2 = "word";
+		String s3 = s1 + " " + s2;
+		System.out.println(s3);
+		String s4 = "to".concat("get").concat("her");
+		System.out.println(s4);
+	}
+
+}
---
+++ String_concat_int.java
@@ -0,0 +1,11 @@
+public class String_concat_int{
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int num = 5;
+		String s5 = "It's "+ num + " days.";
+		System.out.println(s5);
+	}
+
+}
+
