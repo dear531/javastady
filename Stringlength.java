31d07a343bfd91119a1b7c1137938fe813e07058
zhangzly <dear531@21cn.com>
2020-07-28 22:27:08 +0800

    add math length for string

---
+++ Stringlength.java
@@ -0,0 +1,10 @@
+public class Stringlength {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s6 = "1234567";
+		int strlenth = s6.length();
+		System.out.println("s6's length is " + strlenth);
+	}
+
+}
