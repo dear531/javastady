87b2c427c68b4e76b09ca2330afa3d02c7ff3eb9
zhangzly <dear531@21cn.com>
2020-07-29 23:24:45 +0800

    compareTo for strin

---
+++ compareTo.java
@@ -0,0 +1,13 @@
+
+public class compareTo {
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s = "together";
+		String s12 = "ToGetHer";
+		System.out.println(s.compareTo(s12));
+		System.out.println(s.compareToIgnoreCase(s12));
+
+	}
+
+}
+
