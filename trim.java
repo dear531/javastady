34cfbb08562c15f709822057c2729420eb809f41
zhangzly <dear531@21cn.com>
2020-07-29 15:53:59 +0800

    trim Returns a string whose value is this string, with any leading and trailing whitespace removed.

---
+++ trim.java
@@ -0,0 +1,9 @@
+public class trim {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s7 = "   A  B CD  ";
+		System.out.println("\"" + s7 + "\".trim():\"" + s7.trim() + "\"");
+	}
+
+}
