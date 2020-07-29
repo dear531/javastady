5e4b58bcc1546d32c2caab901efecaa28898ead5
zhangzly <dear531@21cn.com>
2020-07-29 15:09:49 +0800

    string for charAt, that Returns the char value at the specified index

---
+++ charAt.java
@@ -0,0 +1,11 @@
+public class charAt {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s6 = "1234567";
+		
+		System.out.println("s6 : " + s6);
+		System.out.println("s6's index 5 is " + s6.charAt(5));
+	}
+
+}
