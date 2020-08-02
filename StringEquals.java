97a7bb1330eab279cf5d9a17707ec64e4e7e82fa
zhangzly <dear531@21cn.com>
2020-08-02 12:29:56 +0800

    StingEquals and EqualsIgnoreCase

---
+++ StringEquals.java
@@ -0,0 +1,13 @@
+public class StringEquals {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s = "together";
+		
+		String s12 = "ToGetHer";
+		System.out.println("\"" + s + "\".equals(\"" + s12 + "\"):" + s.equals(s12));
+		System.out.println("\"" + s + "\".equalsIgnoreCase(\"" + s12 + "\"):" + s.equalsIgnoreCase(s12));
+	
+	}
+
+}
