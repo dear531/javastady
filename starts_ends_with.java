fd849fe905868b2ae31763bd887dcceac05bcfea
zhangzly <dear531@21cn.com>
2020-08-02 12:31:49 +0800

    startsWith and endsWith for string

---
+++ starts_ends_with.java
@@ -0,0 +1,13 @@
+public class starts_ends_with {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s = "together";
+		String s10 = "to";
+		String s11 = "her";
+		System.out.println(s + " is " + s10 + " started:" + s.startsWith(s10));
+		System.out.println(s + " is " + s11 + " ended:" + s.endsWith(s11));
+	
+	}
+
+}
