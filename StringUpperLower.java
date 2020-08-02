68df9879a20448921bf59be43dbc58db0b19227e
zhangzly <dear531@21cn.com>
2020-08-02 12:35:34 +0800

    add string upper and lower for chinese

--- StringUpperLower.java
+++ StringUpperLower.java
@@ -4,6 +4,7 @@ public class StringUpperLower {
 		// TODO 自动生成的方法存根
 		System.out.println("\"AbCdEfG\".toUpperCase():" + "AbCdEfG".toUpperCase());
 		System.out.println("\"AbCdEfG\".toLowerCase():" + "AbCdEfG".toLowerCase());
+		System.out.println("\"中国\".toLowerCase():" + "中国".toLowerCase());
 
 	}
 
