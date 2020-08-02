742f25dc0af47f90835265b8d0830144b42b41e0
zhangzly <dear531@21cn.com>
2020-08-02 12:29:11 +0800

    uppercase and lowercase for string

---
+++ StringUpperLower.java
@@ -0,0 +1,10 @@
+public class StringUpperLower {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("\"AbCdEfG\".toUpperCase():" + "AbCdEfG".toUpperCase());
+		System.out.println("\"AbCdEfG\".toLowerCase():" + "AbCdEfG".toLowerCase());
+
+	}
+
+}
