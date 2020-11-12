513d0f3ba061f6509a0515b078bb89bfddf3b1dd
Leo Cheung <dear531@21cn.com>
2020-11-12 16:17:29 +0800

    optimized bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
 
 	public void sort(int[] array) {
 		for (int i = 1; i < array.length; i++) {
-			for (int j = 0; j < array.length - 1; j++) {
+			for (int j = 0; j < array.length - i; j++) {
 				if (array[j] > array[j + 1]) {
 					int temp = array[j];
 					array[j] = array[j + 1];
