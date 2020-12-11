38f059f64082665d22bbe5f9b0759840bcf6ef14
Leo Cheung <dear531@21cn.com>
2020-12-11 16:59:26 +0800

    add integer max_value, min_value and size

---
+++ GetCon.java
@@ -0,0 +1,16 @@
+
+public class GetCon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxint = Integer.MAX_VALUE;
+		int minint = Integer.MIN_VALUE;
+		int intsize = Integer.SIZE;
+		System.out.println("int类型可取得最大值是" + maxint);
+		System.out.println("int类型可取得最小值是" + minint);
+		System.out.println("int类型得二进制位数是" + intsize);
+
+	}
+
+}
+
