6b0c6ee19314c78e9128be4b35c99e821759d723
Leo Cheung <dear531@21cn.com>
2020-12-11 16:40:26 +0800

    integer toString, toBinaryString, etc

---
+++ Charac.java
@@ -0,0 +1,12 @@
+public class Charac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Integer.toString(456):" + Integer.toString(456));
+		System.out.println("Integer.toBinaryString(456):" + Integer.toBinaryString(456));
+		System.out.println("Integer.toHexString(456):" + Integer.toHexString(456));
+		System.out.println("Integer.toOctalString(456):" + Integer.toOctalString(456));
+	}
+
+}
+
