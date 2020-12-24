e19b9e914e764421c216e3732022541ce9b82207
Leo Cheung <dear531@21cn.com>
2020-12-24 08:25:07 +0800

    add Number Class

---
+++ Number.java
@@ -0,0 +1,20 @@
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Character c = new Character('a');
+		System.out.println(c);
+		System.out.println(c + "是小写字母么" + c.isLowerCase(c));
+
+		Double d = new Double("123.456");
+		System.out.println(d);
+		System.out.println(d.MAX_EXPONENT);
+		System.out.println(d.NEGATIVE_INFINITY);
+
+
+
+	}
+
+}
