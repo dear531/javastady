40e96bab66272776c6268bf7762210200ff238e6
Leo Cheung <dear531@21cn.com>
2020-12-28 15:58:03 +0800

    add 4th part for mothed of math class

---
+++ AnyFunction.java
@@ -0,0 +1,11 @@
+
+public class AnyFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("4和8较大者：" + Math.max(4, 8));
+		System.out.println("4.4和4较小者：" + Math.min(4.4, 4));
+		System.out.println("-7的绝对值：" + Math.abs(-7));
+	}
+
+}
