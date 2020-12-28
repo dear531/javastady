4e8eccd72915b77f5dd54bbc40fcaec942fbd2e0
Leo Cheung <dear531@21cn.com>
2020-12-28 14:47:37 +0800

    add 2nd part of mothed of match class

---
+++ ExponentFunction.java
@@ -0,0 +1,23 @@
+
+public class ExponentFunction {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+//取e的2次方
+System.out.println("e的平方值:" + Math.exp(2));
+//取以e为底2的对数
+System.out.println("以e为底2的对数值:" + Math.log(2));
+//取以10为底2的对数
+System.out.println("以10为底2的对数值:" + Math.log10(2));
+//取4的平方根
+System.out.println("4的平方根:" + Math.sqrt(4));
+//取8的平方根
+System.out.println("8的立方根:" + Math.cbrt(8));
+//取2的2次方
+System.out.println("2的2次方值:" + Math.pow(2, 2));
+
+
+}
+
+}
+
