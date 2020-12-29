20827554413d9cc126247e2d7bd80fd5f416d884
Leo Cheung <dear531@21cn.com>
2020-12-29 16:49:45 +0800

    optimized Subroutine

--- Subroutine.java
+++ Subroutine.java
@@ -1,24 +1,24 @@
+//父类
 class Parent {
 	Parent() {
-		// TODO Auto-generated constructor stub
 		System.out.println("调用父类的Parent()构造方法");
 	}
 }
 
+//继承Parent类
 class SubParent extends Parent {
 	SubParent() {
 		System.out.println("调用子类的SubParent()构造方法");
 	}
 }
 
+//继承SubParent类
 public class Subroutine extends SubParent {
 	Subroutine() {
-		// TODO Auto-generated constructor stub
 		System.out.println("调用子类的Subroutine()构造方法");
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//实例化子类对象
 		Subroutine s = new Subroutine();
 	}
